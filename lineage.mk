a8a6fdaa1289ef034b5a29d5840e34ca1545f676
schwienernitzel <pfelix0803@gmail.com>
2024-09-07 08:46:43 +0000

    device: Inherit wifi-only configurations
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit from device.
 $(call inherit-product, device/huawei/hi3635/device.mk)
